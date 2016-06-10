8d2877955cd51bbcf83864ece43e762603092ba7
blooper05 <legend.of.blooper@gmail.com>
2016-06-11 00:12:12 +0900

    fix common/files permissions

--- cookbooks/common/default.rb
+++ cookbooks/common/default.rb
@@ -1,6 +1,6 @@
 ### AWS CLI ###
 remote_file '/etc/profile.d/aws-cli.conf.sh' do
-  mode '0775'
+  mode '0644'
 end
 
 ### Git ###
@@ -8,5 +8,5 @@ package 'git'
 
 ### Less ###
 remote_file '/etc/profile.d/less.conf.sh' do
-  mode '0775'
+  mode '0644'
 end
