bc7167e53258e92d861d60e7f46ae2b23de6a83f
blooper05 <legend.of.blooper@gmail.com>
2016-06-11 11:00:41 +0900

    update common_spec

--- spec/web/common_spec.rb
+++ spec/web/common_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
+describe command('printenv AWS_DEFAULT_REGION') do
+  let(:disable_sudo) { true }
+  its(:stdout) { is_expected.to match('ap-northeast-1') }
+end
+
 describe package('git') do
-  it { should be_installed }
+  it { is_expected.to be_installed }
+end
+
+describe command('printenv LESS') do
+  let(:disable_sudo) { true }
+  its(:stdout) { is_expected.to match('-inqMRS') }
+end
+
+describe command('printenv LESSCHARSET') do
+  let(:disable_sudo) { true }
+  its(:stdout) { is_expected.to match('utf-8') }
 end
