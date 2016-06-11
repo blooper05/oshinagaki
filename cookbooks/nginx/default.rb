4b34f476b690c22037438493346047b4a586c3d2
blooper05 <legend.of.blooper@gmail.com>
2016-06-11 13:14:11 +0900

    add nginx.repo

--- cookbooks/nginx/default.rb
+++ cookbooks/nginx/default.rb
@@ -1,3 +1,10 @@
+### Yum Repository ###
+remote_file '/etc/yum.repos.d/nginx.repo' do
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
 ### Install ###
 package 'nginx'
 
--- spec/web/nginx_spec.rb
+++ spec/web/nginx_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
+describe yumrepo('nginx') do
+  it { is_expected.to exist }
+  it { is_expected.to be_enabled }
+end
+
 describe package('nginx') do
   it { is_expected.to be_installed }
 end
