6e60622e69fb6e2737c01710814f0812e2f3548c
blooper05 <legend.of.blooper@gmail.com>
2016-06-12 18:20:15 +0900

    add ruby-build

--- cookbooks/ruby/default.rb
+++ cookbooks/ruby/default.rb
@@ -1,10 +1,18 @@
 ### rbenv ###
-git '/usr/local/rbenv' do
+RBENV_PATH = '/usr/local/rbenv'.freeze
+RBENV_CONF = '/etc/profile.d/rbenv.conf.sh'.freeze
+
+git RBENV_PATH do
   repository 'https://github.com/rbenv/rbenv.git'
 end
 
-remote_file '/etc/profile.d/rbenv.conf.sh' do
+remote_file RBENV_CONF do
   owner 'root'
   group 'root'
   mode '0644'
+end
+
+### ruby-build ###
+git "#{RBENV_PATH}/plugins/ruby-build" do
+  repository 'https://github.com/sstephenson/ruby-build.git'
 end
