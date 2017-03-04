05d5f47a29981b9dbf028644ce6b685c905f1672
blooper05 <legend.of.blooper@gmail.com>
2017-03-04 18:44:17 +0900

    :sparkles: Add Node.js

---
+++ cookbooks/nodejs/default.rb
@@ -0,0 +1,7 @@
+### Yum Repository ###
+execute 'curl -sL https://rpm.nodesource.com/setup_6.x | bash -' do
+  not_if 'test -e /etc/yum.repos.d/nodesource-el.repo'
+end
+
+### Install ###
+package 'nodejs'
