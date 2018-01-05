37329eaa90ed855f19fba856ff565d129c926cce
blooper05 <legend.of.blooper@gmail.com>
2018-01-05 17:57:57 +0900

    :+1: Update base cookbooks

--- cookbooks/base/default.rb
+++ cookbooks/base/default.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 ### Yum ###
-execute 'yum clean all'
-execute 'yum -y update'
+execute 'yum clean all && yum -y update' do
+  not_if 'yum check-update'
+end
 
 ### AWS CLI ###
 remote_file '/etc/profile.d/aws-cli.conf.sh' do
